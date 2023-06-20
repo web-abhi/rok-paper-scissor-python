d3d69396b774253fbf8b26d2874ce55a076a7105
Abhishek Singh <66420195+web-abhi@users.noreply.github.com>
2023-06-20 12:56:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+hand = [rock,paper,scissors]
+user = int(input("What do you choose? Type 0 for rock, 1 for Paper, 2 for Scissors.\n"))
+print(hand[user])
+computer = random.randint(0,2)
+print(f"Computer choose:")
+print(hand[computer])
+if user >= 3 or user < 0:
+    print("You input invalid number.")
+elif user == 0 and computer == 2:
+    print("You win!")
+elif user == 2 and computer ==0:
+    print("You loose!")
+elif computer > user:
+    print("You loose!")
+elif user > computer:
+    print("You win!")
+elif computer == user:
+    print("Tie")
